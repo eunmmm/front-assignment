d571a2a4dc73cba1327936e93416d0c1d57891f8
eunmmm <rosiebaek223@gmail.com>
2024-09-11 06:07:02 +0900

    Feat: TodoFormDialog 컴포넌트와 스타일, 이미지 추가

---
+++ src/components/todo/TodoFormDialog/TodoFormDialog.tsx
@@ -0,0 +1,44 @@
+import Dialog from '@/components/ui/Dialog/Dialog';
+import Button from '@/components/ui/Button/Button';
+import TodoForm from '@/components/todo/TodoForm/TodoForm';
+
+import styles from './TodoFormDialog.module.scss';
+
+type TodoFormDialogProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  mode: 'create' | 'update';
+};
+
+const TodoFormDialog = ({
+  isOpen,
+  onClose,
+  mode,
+}: TodoFormDialogProps): JSX.Element => {
+  return (
+    <Dialog isOpen={isOpen} onClose={onClose}>
+      <div className={styles.dialogContent}>
+        <form className={styles.form}>
+          <TodoForm
+            title=""
+            description=""
+            onTitleChange={() => {}}
+            onDescriptionChange={() => {}}
+            formTitle={mode === 'create' ? 'Create Todo' : 'Update Todo'}
+          />
+          <Button
+            text="Submit"
+            theme="primary"
+            size="medium"
+            className={styles.submitButton}
+          />
+        </form>
+        <button type="button" className={styles.closeButton}>
+          <img src="/icon/icon-close.png" alt="Close" />
+        </button>
+      </div>
+    </Dialog>
+  );
+};
+
+export default TodoFormDialog;
