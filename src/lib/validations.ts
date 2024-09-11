d2ad36346bb5f098c507bafdc0d9439006eb2f4f
eunmmm <rosiebaek223@gmail.com>
2024-09-12 02:29:46 +0900

    Feat: Todo 데이터 유효성 검사를 위한 TodoSchema 함수 추가

---
+++ src/lib/validations.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const TodoSchema = z.object({
+  title: z
+    .string()
+    .min(2, '제목은 필수입니다.')
+    .max(100, '제목은 100자를 넘을 수 없습니다.'),
+  description: z.string().max(500, '설명은 500자를 넘을 수 없습니다.'),
+});
+
+export type TodoFormData = z.infer<typeof TodoSchema>;
