467224f81d3fa336a16a6ccd33ecc9f3265609ad
eunmmm <rosiebaek223@gmail.com>
2024-09-11 17:20:54 +0900

    Feat: TodoItem 컴포넌트에 상세 페이지로 이동하는 링크 추가

--- src/components/todo/TodoItem/TodoItem.tsx
+++ src/components/todo/TodoItem/TodoItem.tsx
@@ -1,24 +1,24 @@
+import Link from 'next/link';
+
+import { Todo } from '@/types/todo';
+
 import Checkbox from '@/components/ui/Checkbox/Checkbox';
 import Button from '@/components/ui/Button/Button';
 
 import styles from './TodoItem.module.scss';
 
 type TodoItemProps = {
-  title: string;
-  description: string;
-  completed: boolean;
+  todo: Todo;
 };
 
-const TodoItem = ({
-  title,
-  description,
-  completed,
-}: TodoItemProps): JSX.Element => {
+const TodoItem = ({ todo }: TodoItemProps) => {
   return (
     <section className={styles.todoItem}>
       <div className={styles.todoContent}>
-        <Checkbox label={title} defaultChecked={completed} />
-        <p className={styles.description}>{description}</p>
+        <Checkbox label={todo.title} defaultChecked={todo.completed} />
+        <Link href={`/todo-list/${todo.id}`}>
+          <p className={styles.description}>{todo.description}</p>
+        </Link>
       </div>
       <div className={styles.buttonGroup}>
         <Button text="update" theme="primary" />
