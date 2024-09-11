44d20e05e8000d8c633f3ef9b5a6ce0ec9d0c5b6
eunmmm <rosiebaek223@gmail.com>
2024-09-12 06:22:17 +0900

    Feat: TodoList에 삭제 기능 추가

--- src/app/todo-list/TodoListClient.tsx
+++ src/app/todo-list/TodoListClient.tsx
@@ -5,8 +5,10 @@ import { useState } from 'react';
 import TodoList from '@/components/todo/TodoList/TodoList';
 import Button from '@/components/ui/Button/Button';
 import TodoFormDialog from '@/components/todo/TodoFormDialog/TodoFormDialog';
+import AlertDialog from '@/components/todo/AlertDialog/AlertDialog';
 
 import { Todo } from '@/types/todo';
+import { handleDeleteTodo } from '@/app/todo-list/actions';
 
 import styles from './TodoListPage.module.scss';
 
@@ -18,10 +20,22 @@ const TodoListClient = ({ initialTodos }: TodoListClientProps) => {
   const [todos, setTodos] = useState(initialTodos);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [editTodo, setEditTodo] = useState<Todo | null>(null);
+  const [deleteTodoId, setDeleteTodoId] = useState<string | null>(null);
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
+  const openCreateDialog = () => {
+    setEditTodo(null);
+    setIsDialogOpen(true);
+  };
 
   const handleCreateSuccess = (newTodo: Todo) => {
     setTodos((prevTodos) => [...prevTodos, newTodo]);
     setIsDialogOpen(false);
+  };
+
+  const openUpdateDialog = (todo: Todo) => {
+    setEditTodo(todo);
+    setIsDialogOpen(true);
   };
 
   const handleUpdateSuccess = (updatedTodo: Todo) => {
@@ -34,14 +48,27 @@ const TodoListClient = ({ initialTodos }: TodoListClientProps) => {
     setEditTodo(null);
   };
 
-  const openCreateDialog = () => {
-    setEditTodo(null);
-    setIsDialogOpen(true);
+  const handleDelete = (id: string) => {
+    setDeleteTodoId(id);
+    setIsAlertOpen(true);
   };
 
-  const openUpdateDialog = (todo: Todo) => {
-    setEditTodo(todo);
-    setIsDialogOpen(true);
+  const handleDeleteConfirm = async () => {
+    if (deleteTodoId) {
+      await handleDeleteTodo(deleteTodoId);
+
+      setTodos((prevTodos) =>
+        prevTodos.filter((todo) => todo.id !== deleteTodoId),
+      );
+
+      setDeleteTodoId(null);
+      setIsAlertOpen(false);
+    }
+  };
+
+  const handleDeleteCancel = () => {
+    setDeleteTodoId(null);
+    setIsAlertOpen(false);
   };
 
   const handleDialogClose = () => {
@@ -58,7 +85,11 @@ const TodoListClient = ({ initialTodos }: TodoListClientProps) => {
         onClick={openCreateDialog}
       />
       <div className={styles.todoListWrapper}>
-        <TodoList todos={todos} onUpdate={openUpdateDialog} />
+        <TodoList
+          todos={todos}
+          onUpdate={openUpdateDialog}
+          onDelete={handleDelete}
+        />
       </div>
       <TodoFormDialog
         isOpen={isDialogOpen}
@@ -68,6 +99,12 @@ const TodoListClient = ({ initialTodos }: TodoListClientProps) => {
         initialTitle={editTodo?.title || ''}
         initialDescription={editTodo?.description || ''}
         todoId={editTodo?.id}
+      />
+      <AlertDialog
+        isOpen={isAlertOpen}
+        message="정말 삭제하시겠습니까?"
+        onClose={handleDeleteCancel}
+        onConfirm={handleDeleteConfirm}
       />
     </>
   );
--- src/components/todo/TodoList/TodoList.tsx
+++ src/components/todo/TodoList/TodoList.tsx
@@ -5,13 +5,19 @@ import { Todo } from '@/types/todo';
 interface TodoListProps {
   todos: Todo[];
   onUpdate: (todo: Todo) => void;
+  onDelete: (id: string) => void;
 }
 
-const TodoList = ({ todos, onUpdate }: TodoListProps) => {
+const TodoList = ({ todos, onUpdate, onDelete }: TodoListProps) => {
   return (
     <div>
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onUpdate={onUpdate} />
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+        />
       ))}
     </div>
   );
