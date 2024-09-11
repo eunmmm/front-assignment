85a84a44014f68b47b9935252565d420b8b2e762
eunmmm <rosiebaek223@gmail.com>
2024-09-12 04:35:07 +0900

    Feat: updateTodo API 함수 추가

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -57,3 +57,30 @@ export async function createTodo(data: {
     throw new Error('서버 응답을 처리하는 데 문제가 발생했습니다.');
   }
 }
+
+export async function updateTodo(
+  id: string,
+  data: {
+    title: string;
+    description: string;
+    completed: boolean;
+  },
+): Promise<Todo> {
+  const res = await fetchWithErrorHandling(`${API_URL}/todos/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const todo: Todo = await res.json();
+
+    return todo;
+  } catch (error) {
+    console.error('응답을 JSON으로 변환하는 중 오류:', error);
+
+    throw new Error('서버 응답을 처리하는 데 문제가 발생했습니다.');
+  }
+}
